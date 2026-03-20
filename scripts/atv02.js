cee8dee3225ad6ea7a6053cfd63864cfecd8144f
LsiedC <lucas_correa14@estudante.sesisenai.org.br>
2026-03-20 11:36:34 -0300

    add - fiz a atividade 2 e ja preparei os outros arquivos java-

---
+++ scripts/atv02.js
@@ -0,0 +1,7 @@
+function contagem(){
+    alert("A contagem vai começar!")
+    for(let i = 10; i >= 0; i--){
+        alert("O lançamento vai começar em: " + i);
+    }
+    alert("Lançamento realizado!");
+}
