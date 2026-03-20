f5492bcb78bb3c564d49e9391f09d77bf3d15372
LsiedC <lucas_correa14@estudante.sesisenai.org.br>
2026-03-20 12:21:03 -0300

    add - quase finalizando a atv03

---
+++ scripts/atv03.js
@@ -0,0 +1,25 @@
+function balanco(){
+    let bruto = 0;
+    let gastos =0;
+    let soma_bruto = 0 ;
+    let soma_gastos = 0 ;
+    
+
+    for(i = 0; i < 13; i++){
+        let bruto = parseFloat(prompt("Qual o ganho bruto da empresa no " + (i) + "mês?"));
+        if(!isNaN(bruto)){
+            soma_bruto += bruto;
+            console.log(soma_bruto);
+        }
+        
+        let gasto = parseFloat(prompt("Quais foram o total dos gastos da empresa no " + (i) + "mês?"));
+        if(!isNaN(gasto)){
+            soma_gastos += gastos;
+            console.log(soma_gastos);
+        }
+    }   
+
+    alert("Os seus gastos foram de: " + soma_gastos + "e seus ganhos foram de: " + soma_bruto)
+    alert("O saldo financeiro atual é de: " + (soma_bruto - soma_gastos));
+
+}
