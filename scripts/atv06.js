743a7f2657cc86ae6963174299a3d7bce7833496
LsiedC <lucas_correa14@estudante.sesisenai.org.br>
2026-03-25 11:37:29 -0300

    add  - finalizei a atividade 06

---
+++ scripts/atv06.js
@@ -0,0 +1,9 @@
+function alfabeto(){
+    let letra = prompt("Digite uma letra: ");
+    
+    if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+        alert("Sua letra é uma vogal!");
+    } else {
+        alert("Sua letra é uma consoante!");
+    }
+}
