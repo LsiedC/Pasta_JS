8f13ce068aac557b782ba692b16e477d016c13f2
LsiedC <lucas_correa14@estudante.sesisenai.org.br>
2026-03-25 12:17:27 -0300

    add - agora sim a atividade 08 foi finalizada, assim como a atividade 07

---
+++ scripts/atv08.js
@@ -0,0 +1,15 @@
+function varias_coisas(){
+    let a =  20;
+    let b = 40;
+    let diferenca = 0;
+    let dobro = 0;
+    let multiplicacao = 0;
+
+    alert("Seus dois numeros são: " + a + " e " + b);
+    diferenca = a - b;
+    alert("Seus dois numeros são: " + a + " e " + b + "\nA diferença dos seus numeros é: " + diferenca);
+    dobro = (a*2) + (b*3);
+    alert("Seus dois numeros são: " + a + " e " + b + "\nO dobro da primeira variável somado ao tripo da segunda é: " + dobro);
+    multiplicacao = a*b;
+    alert("Seus dois numeros são: " + a + " e " + b + "\nA multiplicação dos seus numeros é: " + multiplicacao);
+}
