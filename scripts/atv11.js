8e399acafdacc29cd1cb1cd6793daed9b86c99c4
LsiedC <lucas_correa14@estudante.sesisenai.org.br>
2026-03-30 11:09:42 -0300

    add - finalizei a atividade 11

--- scripts/atv11.js
+++ scripts/atv11.js
@@ -7,7 +7,7 @@ function folha_variavel(){
         let salario_descontado = salario - (salario*0.08);
         alert("Seu salario é de: " + salario + ". O leão estava com fome e comeu uma parte! (8%)\nAgora é: " + salario_descontado + "\nO valor do desconto foi de: " + desconto);
 
-        } else if (salario > 1000 || salario < 1500 ) {
+        } else if (salario > 1000 && salario < 1500 ) {
             let desconto = salario*0.085;
             let salario_descontado = salario - (salario*0.085);
             alert("Seu salario é de: " + salario + ". O leão estava com fome e comeu uma parte! (8,5%)\nAgora é: " + salario_descontado + "\nO valor do desconto foi de: " + desconto);
@@ -16,6 +16,5 @@ function folha_variavel(){
                     let desconto = salario*0.09;
                     let salario_descontado = salario - (salario*0.09);
                     alert("Seu salario é de: " + salario + ". O leão estava com fome e comeu uma parte! (9%)\nAgora é: " + salario_descontado + "\nO valor do desconto foi de: " + desconto);
-
                 }
     }
